7d037f1dc1b37761f3787db986f8f19563c35524
Gustavo Sampaio <sampaioglima@gmail.com>
2021-07-22 21:51:52 -0300

    Variables, primitive types and objects.

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let student = {
+    name: 'Gustavo',
+    age: 19,
+    weightInKG: 62,
+    isMinor: false,
+    voterId: undefined,
+    currentJob: null,
+    dateOfRecord: new Date(2021, 07, 22)
+};
 
-console.log("Hello, World.");
+console.log(typeof(student.voterId));
+console.log(typeof(student.currentJob));
+
+let property = 'age';
+// brackets notation
+student[property] = 20;
+
+console.log('Age: ' + student.age);
