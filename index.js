62d9f42fa7d1d198ab13ef7ba49d06879ac4eab1
Gustavo Sampaio <sampaioglima@gmail.com>
2021-07-23 01:17:30 -0300

    Arrays and Functions.

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
-let student = {
-    name: 'Gustavo',
-    age: 19,
-    weightInKG: 62,
-    isMinor: false,
-    voterId: undefined,
-    currentJob: null,
-    dateOfRecord: new Date(2021, 07, 22)
-};
+let greekThinkers = ['Socrates', 'Aristotle', 'Plato', 10];
+console.log(greekThinkers);
 
-console.log(typeof(student.voterId));
-console.log(typeof(student.currentJob));
+// doesn't throw out of bounds exception,
+// returns undefined instead.
+console.log(greekThinkers[3]);
+console.log(greekThinkers.length);
 
-let property = 'age';
-// brackets notation
-student[property] = 20;
+function greet(firstName, lastName) {
+    let timeOfDay;
+    let currentHour = new Date().getHours();
+    if (currentHour >= 0 && currentHour < 12 ) {
+        timeOfDay = 'morning';
+    }
+    else if (currentHour >= 12 && currentHour < 17) {
+        timeOfDay = 'afternoon';
+    }
+    else if (currentHour >= 17 && currentHour < 24) {
+        timeOfDay = 'evening';
+    }
+    console.log(`Good ${timeOfDay}, ${firstName} ${lastName}`);
+}
 
-console.log('Age: ' + student.age);
+greet('Gustavo', 'Sampaio');
+
+function square(number) {
+    return number * number;
+}
+
+console.log(square(4));
