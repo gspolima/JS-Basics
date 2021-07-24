b44bea5d8113498aa662749178ba3a50cc74dd86
Gustavo Sampaio <sampaioglima@gmail.com>
2021-07-23 21:07:32 -0300

    Exercise 1.

--- index.js
+++ index.js
@@ -1,36 +1,10 @@
-function power(base, exponent, usePowerOperator) {
-    if (usePowerOperator === true)
-    {
-        return `${base ** exponent} with power op`;
-    }
-    else
-    {
-        let result = 1;
-        for(let index = 1; index <= exponent; index++)
-        {
-            result *= base;
-        }
-        return `${result} without power op`;
-    }
-}
+// Swapping variables exercise
+let a = 'red';
+let b = 'blue';
 
-console.log(power(2, 9, true));
+let temVar = a;
+a = b;
+b = temVar;
 
-let customer = {
-    name: 'Jesse',
-    points: 107,
-    tier: ''
-};
-
-customer.points > 100 ? customer.tier = 'gold' : customer.tier = 'silver';
-
-console.log(customer.tier);
-
-// 01000110 = 70
-// 10100010 = 162
-
-// Bitwise OR = 11100110 = 230
-console.log(70 | 162);
-
-// Bitwise AND = 00000010 = 2
-console.log(70 & 162);
+console.log(a);
+console.log(b);
