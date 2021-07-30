09186c83a9606fcce5a2e318328d0012f6e937d8
Gustavo Sampaio <sampaioglima@gmail.com>
2021-07-30 17:57:38 -0300

    Objects - exercises.

--- index.js
+++ index.js
@@ -1,63 +1,140 @@
-// [Value and Reference Types]
-let n = 10;
+// [Address object]
+const homeAddress = createAddress('5 Saltoun Rd', 'London', 'SW2 1EN');
+const workAddress = new Address('Abbey Road', 'London', 'NW8 0AE');
 
-function changeNumber(n) {
-    n = 20;
+showAddress('Home', homeAddress);
+showAddress('Work', workAddress);
+
+function showAddress(type, address) {
+    console.log(type);
+    for (const entry of Object.entries(address))
+        console.log(`${entry[0].toUpperCase()}: ${entry[1]}`);
 }
 
-changeNumber(n);
-console.log(n);
-
-
-let obj = {
-    name: 'Cake'
-};
-
-function changeObject(obj) {
-    obj.name = 'Pie';
-//  obj = {};   Won't change anything;
+function createAddress(street, city, zipCode) {
+    return {
+        street,
+        city,
+        zipCode
+    }
 }
 
-changeObject(obj);
-console.log(obj);
-
-// [Iterate through object properties]
-const book = {
-    title: 'An Honest Thief',
-    year: 1848
+function Address(street, city, zipCode) {
+    this.street = street,
+    this.city = city,
+    this.zipCode = zipCode
 }
 
-for (let key in book)
-    console.log(key, book[key]);
-
-for (let key of Object.keys(book))
-    console.log(key);
-
-for (let entry of Object.entries(book))
-    console.log(entry);
-
-// [Cloning]
-// for (const key in book)
-//     anotherBook[key] = book[key];
-
-//const anotherBook = Object.assign(anotherBook, book);
-
-const anotherBook = {...book};
-console.log(anotherBook);
-
-// [Strings]
-const species = 'Homo Sapiens';
-const chars = species.split(' ');
-const result = '';
-for (const value of chars) {
-    console.log(result.concat(value));
+// [Object Equality]
+const address1 = new Address('5th avenue', 'New York', '10110-0001');
+const address2 = new Address('5th avenue', 'New York', '10110-0001');
+const address3 = address1;
+const address4 = {
+    street: '5th avenue',
+    town: 'New York',
+    zipCode: '10110-0001'
 }
 
-// [Dates]
-const now = new Date();
-now.setMonth(7);
-now.setHours(8);
-now.setDate(23);
+console.log(areEqual(address1, address2));
+console.log(areEqual(address1, address4));
+console.log(areSame(address1, address3));
 
-console.log(now.getTimezoneOffset());
-console.log(now.toISOString());
+function areEqual(obj1, obj2) {
+    let result = true;
+    for (const key in obj1)
+        if (obj1[key] !== obj2[key])
+           return false;
+    return result;
+}
+
+function areSame(obj1, obj2) {
+    return obj1 === obj2 ? true : false;
+}
+
+// [Blog post object]
+const blog = {
+    title: '10 Great JavaScript Libraries for Productivity',
+    author: 'Markus Vissers',
+    views: 54,
+    body:
+    `Lorem ipsum dolor sit amet, consectetur 
+      adipiscing elit. Donec elit mi, faucibus 
+      ac feugiat in, rutrum nec urna. Aliquam vehicula.`,
+    comments: [
+        {
+            author: 'John Doe',
+            body: 'Great content!'
+        },
+        {
+            author: 'Gavin Smith',
+            body: `They'll all be very useful.`
+        },
+        {
+            author: 'Jake Keith',
+            body: 'Axios is all I needed.'
+        }
+    ],
+    isLive: true
+}
+
+for (const key in blog) {
+    const element = blog[key];
+    if (element.constructor === Array) {
+        console.log(`${key}:`);
+        for (const comment of Object.entries(element)) {
+            let commentString = '';
+            for (const property of comment)
+                if (typeof(property) !== 'string')
+                    for (const value in property)
+                        if (commentString.length === 0)
+                            commentString += `${property[value]} says `;
+                        else
+                            commentString += `"${property[value]}"`;
+            console.log(commentString);
+        }
+    }
+    else
+        console.log(`${key}: ${element}`);
+}
+
+// [Blog post draft]
+let draft = new Post('sample title', 'Mary Moore', 'Lorem ipsum dolor sit amet.')
+
+console.log(draft);
+
+function Post(title, author, body) {
+    this.title = title,
+    this.author = author,
+    this.body = body,
+    this.views = 0,
+    this.comments = [],
+    this.isLive = false
+}
+
+// [Price ranges]
+const priceLevels = [
+    {
+        label: '$',
+        tooltip: 'Inexpensive',
+        minPerPerson: '4',
+        maxPerPerson: '30'
+    },
+    {
+        label: '$',
+        tooltip: 'Moderate',
+        minPerPerson: '10',
+        maxPerPerson: '80'
+    },
+    {
+        label: '$',
+        tooltip: 'Expensive',
+        minPerPerson: '50',
+        maxPerPerson: '400'
+    }
+]
+
+const restaurants = [
+    { averagePerPerson: 5 },
+    { averagePerPerson: 12 },
+    { averagePerPerson: 34 },
+]
