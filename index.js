1f3406345aca24dca90c57399352f817cf20f441
Gustavo Sampaio <sampaioglima@gmail.com>
2021-07-31 19:35:24 -0300

    Arrays part 1.

--- index.js
+++ index.js
@@ -1,140 +1,61 @@
-// [Address object]
-const homeAddress = createAddress('5 Saltoun Rd', 'London', 'SW2 1EN');
-const workAddress = new Address('Abbey Road', 'London', 'NW8 0AE');
+// [Adding, Searching, Removing, Emptying, Combining and Slicing]
+const numbers = [1, 2, 4, 8, 16, 64];
 
-showAddress('Home', homeAddress);
-showAddress('Work', workAddress);
+numbers.push(128);
+numbers.unshift(0);
+numbers.splice(6, 0, 32);
 
-function showAddress(type, address) {
-    console.log(type);
-    for (const entry of Object.entries(address))
-        console.log(`${entry[0].toUpperCase()}: ${entry[1]}`);
-}
+console.log(numbers);
+console.log(numbers.indexOf(8));
+console.log(numbers.lastIndexOf(16));
+console.log(numbers.includes(128));
 
-function createAddress(street, city, zipCode) {
-    return {
-        street,
-        city,
-        zipCode
-    }
-}
 
-function Address(street, city, zipCode) {
-    this.street = street,
-    this.city = city,
-    this.zipCode = zipCode
-}
+let freeGames = [
+    { id: 1, name: 'Rocket League' },
+    { id: 2, name: 'Fortnite' },
+    { id: 3, name: 'Team Fortress 2' },
+    { id: 4, name: 'Apex Legends' },
+    { id: 5, name: 'Destiny 2' }
+];
 
-// [Object Equality]
-const address1 = new Address('5th avenue', 'New York', '10110-0001');
-const address2 = new Address('5th avenue', 'New York', '10110-0001');
-const address3 = address1;
-const address4 = {
-    street: '5th avenue',
-    town: 'New York',
-    zipCode: '10110-0001'
-}
+const fortniteExists = freeGames.find(game => game.name === 'Fortnite');
+console.log(fortniteExists);
 
-console.log(areEqual(address1, address2));
-console.log(areEqual(address1, address4));
-console.log(areSame(address1, address3));
+const last = freeGames.pop();
+console.log(last);
 
-function areEqual(obj1, obj2) {
-    let result = true;
-    for (const key in obj1)
-        if (obj1[key] !== obj2[key])
-           return false;
-    return result;
-}
+const first = freeGames.shift();
+console.log(first);
 
-function areSame(obj1, obj2) {
-    return obj1 === obj2 ? true : false;
-}
+const deleted = freeGames.splice(2, 1);
+console.log(deleted[0].name);
+console.log(freeGames);
 
-// [Blog post object]
-const blog = {
-    title: '10 Great JavaScript Libraries for Productivity',
-    author: 'Markus Vissers',
-    views: 54,
-    body:
-    `Lorem ipsum dolor sit amet, consectetur 
-      adipiscing elit. Donec elit mi, faucibus 
-      ac feugiat in, rutrum nec urna. Aliquam vehicula.`,
-    comments: [
-        {
-            author: 'John Doe',
-            body: 'Great content!'
-        },
-        {
-            author: 'Gavin Smith',
-            body: `They'll all be very useful.`
-        },
-        {
-            author: 'Jake Keith',
-            body: 'Axios is all I needed.'
-        }
-    ],
-    isLive: true
-}
+// const sameGames = freeGames;
+// freeGames.length = 0;
+// freeGames.splice(0, games.length);
 
-for (const key in blog) {
-    const element = blog[key];
-    if (element.constructor === Array) {
-        console.log(`${key}:`);
-        for (const comment of Object.entries(element)) {
-            let commentString = '';
-            for (const property of comment)
-                if (typeof(property) !== 'string')
-                    for (const value in property)
-                        if (commentString.length === 0)
-                            commentString += `${property[value]} says `;
-                        else
-                            commentString += `"${property[value]}"`;
-            console.log(commentString);
-        }
-    }
-    else
-        console.log(`${key}: ${element}`);
-}
+// console.log(sameGames);
+// console.log(freeGames);
 
-// [Blog post draft]
-let draft = new Post('sample title', 'Mary Moore', 'Lorem ipsum dolor sit amet.')
 
-console.log(draft);
+const paidGames = [
+    { id: 6, name: 'Grand Theft Auto V' },
+    { id: 7, name: 'PUBG' },
+    { id: 8, name: 'Papers, Please' },
+    { id: 9, name: 'Shadow of the Colossus' }
+];
 
-function Post(title, author, body) {
-    this.title = title,
-    this.author = author,
-    this.body = body,
-    this.views = 0,
-    this.comments = [],
-    this.isLive = false
-}
+// const combined = freeGames.concat(paidGames);
+const combined = [...freeGames, ...paidGames];
+console.log(combined);
 
-// [Price ranges]
-const priceLevels = [
-    {
-        label: '$',
-        tooltip: 'Inexpensive',
-        minPerPerson: '4',
-        maxPerPerson: '30'
-    },
-    {
-        label: '$',
-        tooltip: 'Moderate',
-        minPerPerson: '10',
-        maxPerPerson: '80'
-    },
-    {
-        label: '$',
-        tooltip: 'Expensive',
-        minPerPerson: '50',
-        maxPerPerson: '400'
-    }
-]
+const copy = [...combined];
+console.log(copy);
 
-const restaurants = [
-    { averagePerPerson: 5 },
-    { averagePerPerson: 12 },
-    { averagePerPerson: 34 },
-]
+const singlePlayerGames = paidGames.slice(2);
+console.log(singlePlayerGames);
+
+combined[3].name = 'PUBG: Battlegrounds';
+console.log(paidGames[1]);
