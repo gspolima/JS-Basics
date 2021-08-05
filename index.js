075120348090b10eb13fdd24a7e7661e77d28a63
Gustavo Sampaio <sampaioglima@gmail.com>
2021-08-05 20:17:11 -0300

    Arrays - exercises.

--- index.js
+++ index.js
@@ -1,87 +1,123 @@
-// [Joining, Sorting, Testing, Filtering, Mapping and Reducing]
-let numbers = [1, 16, 64, 8, 4, 2];
+// [Array from given numbers]
+const result = arrayFromNumbers(-4, 7);
+console.log(result);
 
-const games = [
-    { id: 1, name: 'Fortnite' },
-    { id: 2, name: 'GTA V' },
-    { id: 3, name: 'PUBG' },
-    { id: 4, name: 'Papers, Please' },
-    { id: 5, name: 'Shadow of the Colossus' }
+function arrayFromNumbers(min, max) {
+    const result = [];
+    for (let i = min; i <= max; i++)
+        result.push(i);
+    return result;
+}
+
+// [Custom includes function]
+const exists = includes(result, -7);
+console.log('Exists:', exists);
+
+function includes(array, searchElement) {
+    return array.some(value => value === searchElement);
+}
+
+// [Remove specific elements from an array]
+const array1 = [1, 2, 3, 4, 5, 6, 7, 5, 3, 10, 3];
+const output1 = except(array1, [3, 5, 6]);
+console.log(output1);
+
+function except(array, excluded) {
+    const result = [];
+    array.forEach(number => {
+        if (!excluded.includes(number))
+            result.push(number);
+    });
+    return result;
+};
+
+// [Move elements]
+const array2 = [1, 2, 3, 4, 5, 6];
+const output2 = move(array2, 5, -3);
+console.log(output2);
+ 
+function move(array, index, offset) {
+    if (offset === 0)
+        return array;
+
+    const newPosition = index + offset;
+    if (newPosition >= array.length || newPosition < 0) {
+        console.error('Invalid offset');
+        return;
+    }
+
+    const toMove = array.splice(index, 1);
+    const result = [...array];
+    result.splice(newPosition, 0, toMove[0]);
+    return result;
+}
+
+// [Count occurrences]
+const array3 = [1, 2, 3, 7, 2, 5, 6, 2, 8, 7, 10, 2, 4, 7, 6];
+const output3 = countOccurrences(array3, 2);
+console.log(`Total occurrences: ${output3}`);
+
+function countOccurrences(array, searchElement) {
+//     let count = 0;
+//     while (array.length >= 1) {
+//         const index = array.indexOf(searchElement);
+//         if (index === -1)
+//             break;
+
+//         count += 1;
+//         array = array.slice((index + 1));
+//     }
+//     return count;
+
+    return array.reduce((accumulator, currentValue) => {
+        if (currentValue === searchElement)
+            return accumulator += 1;
+        return accumulator;
+    }, 0);
+}
+
+// [Get max value]
+const array4 = [91, 50, 13, 9];
+const output4 = getMax(array4);
+console.log('Smaller value:', output4);
+
+function getMax(array) {
+    if (array.length === 0)
+        return undefined;
+
+    let minValue = Number.MAX_VALUE;
+    // array.forEach(value => {
+    //     value < minValue ? minValue = value : minValue
+    // });
+    // return minValue;
+
+    return array.reduce((accumulator, currentValue) => {
+        if (currentValue < accumulator)
+            return accumulator = currentValue;
+        return accumulator;
+    }, minValue);
+}
+
+// [List movies from 2018 rated above 4]
+const movies = [
+    { title: 'Mortal Engines', year: 2018, rating: 4.5 },
+    { title: 'Ashes of Snow', year: 2018, rating: 4.7 },
+    { title: 'The Conductor', year: 2018, rating: 3 },
+    { title: 'Disobedience', year: 2017, rating: 4.1 }
 ];
 
-numbers.forEach((number, index) => console.log(`${index}: ${number}`));
+console.log('---');
+listMovies(movies);
 
-const joined = numbers.join(', ');
-console.log(joined);
+function listMovies(array) {
+    array
+        .filter(m => m.rating > 4 && m.year === 2018)
+        .sort(sortMoviesByRatingDesc)
+        .forEach(m => console.log(m.title));
+}
 
-const words = 'A picture is worth a THOUSAND words'.split(' ');
-console.log(words);
-
-const urlSlug = words.join('-').toLowerCase();
-console.log(urlSlug);
-
-
-numbers.sort();
-numbers.sort((x, y) => {
-    if (x > y) return 1;
-    if (x < y) return -1;
+function sortMoviesByRatingDesc(m1, m2) {
+    if (m1.rating < m2.rating) return 1;
+    if (m1.rating > m2.rating) return -1;
     return 0;
-});
-
-console.log(numbers);
-
-games.sort((game1, game2) => {
-    const name1 = game1.name.toUpperCase();
-    const name2 = game2.name.toUpperCase();
-
-    if (name1 > name2) return 1;
-    if (name1 < name2) return -1;
-    return 0;
-});
-
-console.log(games);
-
-
-const onlyPositive = numbers.every(n => n > 0);
-console.log(onlyPositive);
-
-const atLeastOneStartsWithS = games.some((g) => {
-    return g.name.toUpperCase().startsWith('S')
-});
-console.log(atLeastOneStartsWithS);
-
-
-const greaterThanFive = numbers.filter(value => value > 5);
-console.log(greaterThanFive);
-
-const startingWithP = games.filter(g => {
-    return g.name.toUpperCase().startsWith('P')
-});
-console.log(startingWithP);
-
-
-const items = numbers
-    .filter(number => number < 10)
-    .sort((x, y) => {
-        if (x > y) return 1;
-        if (x < y) return -1;
-        return 0;
-    })
-    .map(number => ({ value: number }))
-    .filter(obj => obj.value % 2 === 0);
-
-console.log(items);
-
-
-const sum = numbers.reduce(
-    (accumulator, currentValue) => accumulator + currentValue);
-console.log(sum);
-
-// const uniqueGameName = games.reduce((accumulator, currentGame) => {
-//     for (let char of currentGame.name)
-//         if (!accumulator.name.includes(char))
-//             accumulator.name += char;
-//     return accumulator;
-// });
-
-// console.log(uniqueGameName.name);
+}
